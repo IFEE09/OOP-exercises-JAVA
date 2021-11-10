5beeb36bd0823b68960b7a0d37d4619051c2396d
IFEE09 <ivanfaridespadas@gmail.com>
2021-11-10 16:24:09 -0600

    agregando atributos y metodo constructuro a la clase libro

---
+++ libros-poo/src/main/java/Libros.java
@@ -0,0 +1,22 @@
+
+
+/* @author IFEE09 */
+public class Libros {
+    
+    //atributos
+    private int ISBN;
+    private String titulo;
+    private String autor;
+    private short numPaginas;
+    
+    //Metodo constructor
+    public Libros(int ISBN, String titulo, String autor, short numPaginas){
+        this.ISBN = ISBN;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numPaginas = numPaginas;
+        
+    }
+    
+
+}

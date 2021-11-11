36f14b0d64e4cb6edbb8b657328ff897efedce76
IFEE09 <ivanfaridespadas@gmail.com>
2021-11-10 23:21:14 -0600

    arreglando el bug del output

--- 2-constructor-copia/src/main/java/Main.java
+++ 2-constructor-copia/src/main/java/Main.java
@@ -7,13 +7,15 @@ public class Main {
         Contador cuenta_1 = new Contador(20);
         Contador cuenta_2 = new Contador(-20);
         Contador cuenta_3 = new Contador();
-       
+        Contador cuenta_4 = new Contador(cuenta_1);
+        
         cuenta_1.aumentarCuenta();
-        //cuenta_2.decrementarCuenta();
+        cuenta_2.decrementarCuenta();
         
         System.out.println(cuenta_1.getCuenta());
         System.out.println(cuenta_2.getCuenta());
-        System.err.println(cuenta_3.getCuenta());
+        System.err.println(cuenta_3.getCuenta()); //observamos que el constructor sin parametros da por defecto al atributo cuenta el valor de 0
+        System.out.println(cuenta_4.getCuenta());
         
     }
 

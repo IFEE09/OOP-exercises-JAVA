cce4fb740284737f9449a606086d3f45976e131b
IFEE09 <ivanfaridespadas@gmail.com>
2021-11-10 20:35:09 -0600

    subiendo el segundo ejercicio sin instrucciones

---
+++ 2-constructor-copia/src/main/java/Main.java
@@ -0,0 +1,20 @@
+
+/* @author IFEE09 */
+public class Main {
+    
+    public static void main(String args[]){
+        
+        Contador cuenta_1 = new Contador(20);
+        Contador cuenta_2 = new Contador(-20);
+        Contador cuenta_3 = new Contador();
+       
+        cuenta_1.aumentarCuenta();
+        //cuenta_2.decrementarCuenta();
+        
+        System.out.println(cuenta_1.getCuenta());
+        System.out.println(cuenta_2.getCuenta());
+        System.err.println(cuenta_3.getCuenta());
+        
+    }
+
+}

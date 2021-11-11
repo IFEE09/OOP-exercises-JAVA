c6314f55687ac74a77d21c35288bffe5cd110da2
IFEE09 <ivanfaridespadas@gmail.com>
2021-11-11 03:35:05 -0600

    arreglando nombres de los objetos creados

--- 2-constructor-copia/src/main/java/Main.java
+++ 2-constructor-copia/src/main/java/Main.java
@@ -4,19 +4,21 @@ public class Main {
     
     public static void main(String args[]){
         
-        Contador cuenta_1 = new Contador(20);
-        Contador cuenta_2 = new Contador(-20);
-        Contador cuenta_3 = new Contador();
-        Contador cuenta_4 = new Contador(cuenta_1);
+        Contador contador_1 = new Contador(20);
+        Contador contador_2 = new Contador(-20);
+        Contador contador_3 = new Contador();
+        Contador contador_4 = new Contador(contador_1);
         
-        cuenta_1.aumentarCuenta();
-        cuenta_2.decrementarCuenta();
+        contador_1.aumentarCuenta();
+        contador_2.decrementarCuenta();
         
-        System.out.println(cuenta_1.getCuenta());
-        System.out.println(cuenta_2.getCuenta());
-        //observamos que el constructor sin parametros da por defecto al atributo cuenta el valor de 0
-        System.err.println(cuenta_3.getCuenta()); 
-        System.out.println(cuenta_4.getCuenta());
+        System.out.println(contador_1.getCuenta());
+        System.out.println(contador_2.getCuenta());
+        //aqui vemos que el atributo cuenta de la cuenta 3, se inicializa por defecto en 0 
+        System.out.println(contador_3.getCuenta()); 
+        //Aqui vemos que a pesar de que se aumento la cuenta 1, no aumenta la cuenta cuatro que contenia
+        //como parametro a la cuenta_1, ya que no son el mismo objeto, solo le dio sus valores en ese momento de creacion
+        System.out.println(contador_4.getCuenta());
         
     }
 

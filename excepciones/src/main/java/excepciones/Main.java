9d067bde11d1a73a4011132d4357974b24b26379
IFEE09 <ivanfaridespadas@gmail.com>
2022-01-17 01:45:09 -0600

    agregando excepciones

---
+++ excepciones/src/main/java/excepciones/Main.java
@@ -0,0 +1,26 @@
+//Excepciones verificadas (IOException), leyendo un archivo de texto y mostrando su contenido
+package excepciones;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+/* @author IFEE09 */
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        
+        //Leyendo archivo si es que existe, si no existe, tira una excepcion 
+        BufferedReader required = new BufferedReader(new FileReader("/home/ifee09/Repositories/ejercicios-poo/excepciones/required.txt"));
+        String contenido;
+        
+        //Checando que el archivo no este vacio
+        while((contenido = required.readLine()) != null){
+            System.out.println(contenido);
+        }
+        
+        
+    }
+
+}
